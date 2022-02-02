5bef631b517481c4a157646bd720ebed2b190008
hmk10 <halitmk@gmail.com>
2022-02-01 19:54:13 -0500

    Base Driver

---
+++ src/Test/_01_ValidateMenu.java
@@ -0,0 +1,14 @@
+package Test;
+
+import Utils.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _01_ValidateMenu extends BaseDriver {
+
+    @Test
+    public void validateMenuTest() {
+
+
+    }
+
+}

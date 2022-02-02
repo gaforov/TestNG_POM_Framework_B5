76e4a2ca695ba1224bb7d22c2e37883c96afb58a
hmk10 <halitmk@gmail.com>
2022-02-01 20:16:21 -0500

    Base Driver

--- src/Utils/BaseDriver.java
+++ src/Utils/BaseDriver.java
@@ -1,6 +1,8 @@
 package Utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseDriver {
 
-    WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
@@ -19,6 +21,25 @@ public class BaseDriver {
         driver.manage().window().maximize();
 
         driver.get("http://opencart.abstracta.us/index.php?route=common/home");
+
+        WebElement myAccountButton = driver.findElement(By.xpath("//a[@title='My Account']"));
+        myAccountButton.click();
+
+        WebElement loginButton = driver.findElement(By.linkText("Login"));
+        loginButton.click();
+
+        WebElement usernameInput = driver.findElement(By.id("input-email"));
+        usernameInput.sendKeys("test@technostudy.com");
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys("Test12345..");
+
+        WebElement submitLoginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+        submitLoginButton.click();
+
+        WebElement homePageButton = driver.findElement(By.xpath("//a[text()='Your Store']"));
+        homePageButton.click();
+
     }
 
     @AfterMethod

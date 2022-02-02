fe64de390bb8e5295b3a50a9d1a0322469f5be10
hmk10 <halitmk@gmail.com>
2022-02-01 21:24:20 -0500

    SubscribeFunction Test

--- src/Test/_01_SubscribeFunction.java
+++ src/Test/_01_SubscribeFunction.java
@@ -1,7 +1,9 @@
 package Test;
 
 import POM.HomePageElements;
+import POM.MyAccountPageElements;
 import Utils.BaseDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class _01_SubscribeFunction extends BaseDriver {
@@ -10,10 +12,18 @@ public class _01_SubscribeFunction extends BaseDriver {
     public void subscribeFunctionTest() {
 
         HomePageElements homePageElements = new HomePageElements(driver);
+        MyAccountPageElements myAccountPageElements = new MyAccountPageElements(driver);
 
         homePageElements.myAccountIcon.click();
         homePageElements.myAccountButton.click();
 
+        myAccountPageElements.newsletterButton.click();
+        myAccountPageElements.subscribeNewsletterButton.click();
+        myAccountPageElements.continueButton.click();
+
+        String actualMessage = myAccountPageElements.successMessage.getText();
+
+        Assert.assertTrue(actualMessage.contains("Success"));
     }
 
 }

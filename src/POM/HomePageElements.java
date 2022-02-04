30e49ac1e5bdec1d90530eedfe18c3e1f9d69b2e
hmk10 <halitmk@gmail.com>
2022-02-03 21:43:17 -0500

    POM Elements

--- src/POM/HomePageElements.java
+++ src/POM/HomePageElements.java
@@ -2,8 +2,11 @@ package POM;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class HomePageElements {
 
@@ -16,5 +19,15 @@ public class HomePageElements {
 
     @FindBy(xpath = "//a[text()='My Account']")
     public WebElement myAccountButton;
+
+    @FindBy(xpath = "//input[@name='search']")
+    public WebElement searchInput;
+
+    @FindBy(xpath = "//div[@id='search']//button")
+    public WebElement searchButton;
+
+    // @FindAll works like -> driver.findElements()   |   @FindBy works like -> driver.findElement()
+    @FindAll({@FindBy(xpath = "//h4")})
+    public List<WebElement> searchResultItems;
 
 }

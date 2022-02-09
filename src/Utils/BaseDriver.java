4c67a2dcb4afe1ed262f9c47a9dbac4ce368935e
hmk10 <halitmk@gmail.com>
2022-02-08 20:17:31 -0500

    Cross-Browser Testing (@Parameters)

--- src/Utils/BaseDriver.java
+++ src/Utils/BaseDriver.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +16,17 @@ public class BaseDriver {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
-        driver = new ChromeDriver();
+    @Parameters({"browser"})
+    public void setup(String browser) {
+
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/Library/Selenium/geckodriver/geckodriver");
+            driver = new FirefoxDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
